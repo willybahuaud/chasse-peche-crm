c9fb1acf9302a6e591b75282db52451476569046
Willy Bahuaud <willy@wabeo.fr>
2013-02-16 03:33:11 +0100

    good metaboxes

--- wp-content/plugins/easymusic/metaboxes.php
+++ wp-content/plugins/easymusic/metaboxes.php
@@ -29,106 +29,103 @@ SAVES METABOXES
 * @uses em_save_metaboxes FUNCTION to save metaboxes
 */
 function em_save_metaboxes( $post_ID ) { 
-	if( ( !defined( 'DOING_AJAX' ) || !DOING_AJAX ) )return $post_ID;
 
-	if( isset( $_POST[ 'em_genre' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_genre' ] );
-		update_post_meta( $post_ID, '_em_genre', $em_genre );
-	}
+		if( isset( $_POST[ 'em_genre' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$em_genre = sanitize_text_field( $_POST[ 'em_genre' ] );
+			update_post_meta( $post_ID, '_em_genre', $em_genre );
+		}
 
 		if( isset( $_POST[ 'em_titre' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_titre' ] );
-		update_post_meta( $post_ID, '_em_titre', $titre );
-	}
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$em_titre = sanitize_text_field( $_POST[ 'em_titre' ] );
+			update_post_meta( $post_ID, '_em_titre', $em_titre );
+		}
 
-	if( isset( $_POST[ 'em_artiste' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_artiste' ] );
-		update_post_meta( $post_ID, '_em_artiste', $artiste );
-	}
+		if( isset( $_POST[ 'em_artiste' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$artiste = sanitize_text_field( $_POST[ 'em_artiste' ] );
+			update_post_meta( $post_ID, '_em_artiste', $artiste );
+		}
 
-	if( isset( $_POST[ 'em_album' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_album' ] );
-		update_post_meta( $post_ID, '_em_album', $album );
-	}
+		if( isset( $_POST[ 'em_album' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$album = sanitize_text_field( $_POST[ 'em_album' ] );
+			update_post_meta( $post_ID, '_em_album', $album );
+		}
 
-	if( isset( $_POST[ 'em_annee_album' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_annee_album' ] );
-		update_post_meta( $post_ID, '_em_annee_album', $annee_album );
-	}
+		if( isset( $_POST[ 'em_annee_album' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$annee_album = sanitize_text_field( $_POST[ 'em_annee_album' ] );
+			update_post_meta( $post_ID, '_em_annee_album', $annee_album );
+		}
 
-	if( isset( $_POST[ 'em_num_piste' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_num_piste' ] );
-		update_post_meta( $post_ID, '_em_num_piste', $num_piste );
-	}
+		if( isset( $_POST[ 'em_num_piste' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$num_piste = sanitize_text_field( $_POST[ 'em_num_piste' ] );
+			update_post_meta( $post_ID, '_em_num_piste', $num_piste );
+		}
 
-	if( isset( $_POST[ 'em_duree' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_duree' ] );
-		update_post_meta( $post_ID, '_em_duree', $em_duree );
-	}
+		if( isset( $_POST[ 'em_duree' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$em_duree = sanitize_text_field( $_POST[ 'em_duree' ] );
+			update_post_meta( $post_ID, '_em_duree', $em_duree );
+		}
 
-	if( isset( $_POST[ 'em_vitesse' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_vitesse' ] );
-		update_post_meta( $post_ID, '_em_vitesse', $em_vitesse );
-	}
+		if( isset( $_POST[ 'em_vitesse' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$em_vitesse = sanitize_text_field( $_POST[ 'em_vitesse' ] );
+			update_post_meta( $post_ID, '_em_vitesse', $em_vitesse );
+		}
 
-	if( isset( $_POST[ 'em_editeur' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_editeur' ] );
-		update_post_meta( $post_ID, '_em_editeur', $em_editeur );
-	}
+		if( isset( $_POST[ 'em_provider' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$em_provider = sanitize_text_field( $_POST[ 'em_provider' ] );
+			update_post_meta( $post_ID, '_em_provider', $em_provider );
+		}
 
-	if( isset( $_POST[ 'em_date_mise_en_ligne' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_date_mise_en_ligne' ] );
-		update_post_meta( $post_ID, '_em_date_mise_en_ligne', $em_date_mise_en_ligne );
-	}
+		if( isset( $_POST[ 'em_date_mise_en_ligne' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$em_date_mise_en_ligne = sanitize_text_field( $_POST[ 'em_date_mise_en_ligne' ] );
+			update_post_meta( $post_ID, '_em_date_mise_en_ligne', $em_date_mise_en_ligne );
+		}
 
-	if( isset( $_POST[ 'em_format' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_format' ] );
-		update_post_meta( $post_ID, '_em_format', $em_format );
-	}
+		if( isset( $_POST[ 'em_format' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$em_format = sanitize_text_field( $_POST[ 'em_format' ] );
+			update_post_meta( $post_ID, '_em_format', $em_format );
+		}
 
-	if( isset( $_POST[ 'em_encode' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_encode' ] );
-		update_post_meta( $post_ID, '_em_encode', $em_encode );
-	}
+		if( isset( $_POST[ 'em_encode' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$em_encodage = sanitize_text_field( $_POST[ 'em_encode' ] );
+			update_post_meta( $post_ID, '_em_encode', $em_encodage );
+		}
 
-	if( isset( $_POST[ 'em_awards' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_awards' ] );
-		update_post_meta( $post_ID, '_em_awards', $em_awards );
-	}
+		if( isset( $_POST[ 'em_awards' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$em_awards = sanitize_text_field( $_POST[ 'em_awards' ] );
+			update_post_meta( $post_ID, '_em_awards', $em_awards );
+		}
 
 
 		if( isset( $_POST[ 'em_transcript' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_transcript' ] );
-		update_post_meta( $post_ID, '_em_transcript', $em_genre );
-	}
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$transcript = sanitize_text_field( $_POST[ 'em_transcript' ] );
+			update_post_meta( $post_ID, '_em_transcript', $transcript );
+		}
 
 		if( isset( $_POST[ 'em_playlists' ] ) ) {
-		check_admin_referer( 'em_playlists-save_' . $_POST[ 'post_ID' ], 'em_playlists-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_playlists' ] );
-		update_post_meta( $post_ID, 'em_playlists', $em_genre );
-	}
+			check_admin_referer( 'em_playlists-save_' . $_POST[ 'post_ID' ], 'em_playlists-nonce' );
+			$playlists = sanitize_text_field( $_POST[ 'em_playlists' ] );
+			update_post_meta( $post_ID, '_em_playlists', $playlists );
+		}
 
-			if( isset( $_POST[ 'em_url' ] ) ) {
-		check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
-		$em_genre = sanitize_text_field( $_POST[ 'em_url' ] );
-		update_post_meta( $post_ID, 'em_url', $em_genre );
-	}
-
-
+		if( isset( $_POST[ 'em_url' ] ) ) {
+			check_admin_referer( 'em_songs-save_' . $_POST[ 'post_ID' ], 'em_songs-nonce' );
+			$url = sanitize_text_field( $_POST[ 'em_url' ] );
+			update_post_meta( $post_ID, '_em_url', $url );
+		}
 }
 add_action( 'save_post', 'em_save_metaboxes' ); 
 
@@ -140,15 +137,15 @@ function em_songs($post){
 	$titre= get_post_meta($post->ID,'_em_titre',true);
 	$artiste = get_post_meta($post->ID,'_em_artiste',true);
 	$album = get_post_meta($post->ID,'_em_album',true);
-	$annee_album = get_post_meta($post->ID,'_em_annee_alumb',true);
-	$num_piste = get_post_meta($post->ID,'em_num_piste',true);
-	$duree = get_post_meta($post->ID,'em_duree',true);
-	$vitesse = get_post_meta($post->ID,'em_vitesse',true);
-	$editeur = get_post_meta($post->ID,'em_provider',true);
-	$date_mise_en_ligne = get_post_meta($post->ID,'em_date_mise_en_ligne',true);
-	$format = get_post_meta($post->ID,'em_format',true);
-	$encode = get_post_meta($post->ID,'em_encode',true);
-	$awards = get_post_meta($post->ID,'em_awards',true);
+	$annee_album = get_post_meta($post->ID,'_em_annee_album',true);
+	$num_piste = get_post_meta($post->ID,'_em_num_piste',true);
+	$duree = get_post_meta($post->ID,'_em_duree',true);
+	$vitesse = get_post_meta($post->ID,'_em_vitesse',true);
+	$editeur = get_post_meta($post->ID,'_em_provider',true);
+	$date_mise_en_ligne = get_post_meta($post->ID,'_em_date_mise_en_ligne',true);
+	$format = get_post_meta($post->ID,'_em_format',true);
+	$encode = get_post_meta($post->ID,'_em_encode',true);
+	$awards = get_post_meta($post->ID,'_em_awards',true);
 
 
 
@@ -161,7 +158,7 @@ function em_songs($post){
 	echo 'Année Album : <input type="date" name="em_annee_album" value="'.$annee_album.'"><br />';
 	echo 'Numéro de Piste : <input type="int" name="em_num_piste" value="'.$num_piste.'" placeholder="num_piste"><br />';
 	echo 'Durée : <input type="text" name="em_duree" value="'.$duree.'" placeholder="duree"><br />';
-	echo 'Vitesse : <input type="text" name="em_annee_album" value="'.$vitesse.'" placeholder="Vitesse"><br />';
+	echo 'Vitesse : <input type="text" name="em_vitesse" value="'.$vitesse.'" placeholder="Vitesse"><br />';
 	echo 'Editeur : <input type="text" name="em_provider" value="'.$editeur.'" placeholder="Editeur"><br />';
 	echo 'Date de mise en ligne : <input type="date" name="em_date_mise_en_ligne" value="'.$date_mise_en_ligne.'"><br />';
 	echo 'Format : <input type="texte" name="em_format" value="'.$format.'"><br />';
